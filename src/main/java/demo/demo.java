84c078b94bc393691b5f219304fb5185db20ad26
shiyaming <1085574583@qq.com>
2018-08-28 01:28:42 +0800

    开始测试

--- src/main/java/demo/demo.java
+++ src/main/java/demo/demo.java
@@ -12,6 +12,7 @@ public class demo {
 		int num = (int) (Math.random() * 1000000);
 		System.out.println(num);
 		System.out.println(num);
+		System.out.println(num);
 
 		for (int i = 0; i < 1000; i++) {
 			int nextInt = new Random().nextInt(1000000);
