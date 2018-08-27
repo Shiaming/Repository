a4b22f768803b0f014e8b30f085dc6b1a23103df
shiyaming <1085574583@qq.com>
2018-08-28 00:29:30 +0800

    试试

--- src/main/java/demo/demo.java
+++ src/main/java/demo/demo.java
@@ -11,6 +11,7 @@ public class demo {
 	public static void main(String[] args) {
 		int num = (int) (Math.random() * 1000000);
 		System.out.println(num);
+		System.out.println(num);
 
 		for (int i = 0; i < 1000; i++) {
 			int nextInt = new Random().nextInt(1000000);
