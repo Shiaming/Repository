dc9f43bb4c241ee6951398b8e2d5a28f037373b1
shiyaming <1085574583@qq.com>
2018-08-28 01:39:35 +0800

    第一次测试

--- src/main/java/demo/demo.java
+++ src/main/java/demo/demo.java
@@ -1,4 +1,4 @@
-package demo;
+ package demo;
 
 import java.util.Random;
 
@@ -10,15 +10,16 @@ public class demo {
 
 	public static void main(String[] args) {
 		int num = (int) (Math.random() * 1000000);
+		//删除了1
 		System.out.println(num);
 		System.out.println(num);
-		System.out.println(num);
+		
 
 		for (int i = 0; i < 1000; i++) {
 			int nextInt = new Random().nextInt(1000000);
 			if ((nextInt + "").length() == 6) {
 
-				System.out.println("六位随机数: " + nextInt);
+				System.out.println("鍏綅闅忔満鏁�: " + nextInt);
 			}
 
 		}
